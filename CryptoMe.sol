cec2700d604fe076a01c2ed42407c85dcde7ceb1
pierce403 <pierce403@gmail.com>
2018-02-23 06:53:29 -0800

    adding name legitness and payments

--- CryptoMe.sol
+++ CryptoMe.sol
@@ -1,9 +1,6 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.20;
 
 // TODO
-// setprimary()
-// check name legitness
-// add payable limiter
 //
 // auction??
 
@@ -21,29 +18,62 @@ contract CryptoMe {
     }
 
     address admin;
-
+    uint256 basePrice;
+ 
     mapping(string => string) addrs; // map names to addrs    
     mapping(string => identity) ids; // map addrs to ids
+ 
+    string[] names;
  
   function CryptoMe() public
   {
     admin=msg.sender;
+    basePrice=0;
   }
 
-  function register(string name) public returns(bool) {
+  function register(string name) payable public returns(string message, uint256 price, uint256 value) {
+  
+      //if(msg.value < basePrice/bytes(name).length)return "need more eth";
+      //
+      if(msg.value < basePrice)return ("need more eth",basePrice,msg.value);
+      
+      return (registerInternal(name),basePrice,msg.value);
+  }
+  
+  function setBase(uint price) public returns(string message){
+      if(msg.sender!=admin)return "who dis?";
+      
+      basePrice=price;
+      return "awesome";
+  }
+
+  function getBase() constant public returns(uint){
+      return basePrice;
+  }
+
+  function registerInternal(string name) private returns(string message) {
     
       // TODO verify legit name here:
+      if(bytes(name).length>20)
+      {
+          return "name too long";
+      }
+
+      if(!legitName(name))
+      {
+          return "name has bad characters (lower alphanum only)";
+      }
     
       // make sure we haven't seen this name before
       if(bytes(addrs[name]).length!=0)
       {
-          return false;
+          return "name already taken";
       }
-
+      
       // get string value of address    
       string memory addr_s = ascii(msg.sender);
-      identity lookup = ids[addr_s];
-      
+      identity storage lookup = ids[addr_s];
+
       // does this address already have an identity?
       if(lookup.addr != address(0x0))
       {
@@ -53,22 +83,34 @@ contract CryptoMe {
           ids[addr_s]=lookup;
           addrs[name]=addr_s;
           
-          return true;
+          names.push(name);
+          return "successfuly added new alias";
       }
 
-      identity storage newid; //= identity(msg.sender,addr_s,name,"email","ipfs",aliases,connections);
-      newid.addr = msg.sender;
-      newid.addr_s = addr_s;
+      //string[] storage aliases;// = string[](name);
+      //aliases.push(name);
+      
+      //string[] memory connections;
+      
+      // create the new identity
+      identity storage newid; // = identity(msg.sender,addr_s,name,"email","ipfs",aliases,connections);
+      newid.addr=msg.sender;
+      newid.addr_s=addr_s;
       newid.name=name;
-      newid.email="email";
-      newid.ipfs="ipfs";
       newid.aliases.push(name);
       
+      // load identity into registries
       ids[addr_s]=newid;
       addrs[name]=addr_s;
       addrs[addr_s]=addr_s;
-      
-      return true;
+
+      names.push(name);      
+      return "successfully added new identity";
+    }
+
+    function getName(uint offset) public constant returns (string name, uint256 total)
+    {
+        return (names[offset],names.length);
     }
     
     // primary identity lookup function
@@ -76,6 +118,10 @@ contract CryptoMe {
       identity memory id = ids[addrs[name]];
       
       return (id.name,id.addr_s,id.email,id.ipfs,id.aliases.length, id.connections.length);
+    }
+    
+    function getAdmin() public constant returns (address adminaddr){
+        return admin;
     }
     
     // id'd like to return all of them at once,
@@ -98,6 +144,24 @@ contract CryptoMe {
       return true;
     }
     
+    function setPrimary(string name) public returns (string message){
+        
+        // look up identity
+        identity memory id = ids[ascii(msg.sender)];
+        
+        // verify user owns the name
+        for(uint x=0;x<id.aliases.length;++x){
+          if(keccak256(id.aliases[x])==keccak256(name)){
+            id.name=name;
+            ids[id.addr_s]=id;
+            addrs[name]=id.addr_s;
+            return "name update successful";
+          }
+        }
+        
+        return "that name doesn't belong to you";
+    }
+    
     function addConnection(string connection) public returns(bool){
      
       // is this connection a real user?
@@ -116,7 +180,7 @@ contract CryptoMe {
     
     // thanks tkeber
     // https://ethereum.stackexchange.com/questions/8346/convert-address-to-string
-    function ascii(address x) internal returns (string) {
+    function ascii(address x) pure internal returns (string) {
     bytes memory s = new bytes(40);
     for (uint i = 0; i < 20; i++) {
         byte b = byte(uint8(uint(x) / (2**(8*(19 - i)))));
@@ -128,8 +192,35 @@ contract CryptoMe {
     return string(s);
     }
 
-    function char(byte b) internal returns (byte c) {
+    function char(byte b) pure internal returns (byte c) {
       if (b < 10) return byte(uint8(b) + 0x30);
       else return byte(uint8(b) + 0x57);
+    }
+    
+    function legitName(string s) pure private returns (bool legit){
+        
+        bytes memory sbytes=bytes(s);
+        if(sbytes.length>20)return false;
+        
+        // 48 - 57  numeric
+        // 97 - 122 lower alpha
+        for(uint x=0;x<sbytes.length;++x){
+            if((sbytes[x]<48||sbytes[x]>57)&&(sbytes[x]<97||sbytes[x]>122))return false;
+        }
+        
+        return true;
+    }
+    
+    function legitEmail(string s) pure private returns (bool legit){
+        bytes memory sbytes=bytes(s);
+        if(sbytes.length>40)return false;
+        return true;
+    }
+    
+    function legitIPFS(string s) pure private returns (bool legit){
+        
+        bytes memory sbytes=bytes(s);
+        if(sbytes.length!=46)return false;
+        return true;
     }
 }
